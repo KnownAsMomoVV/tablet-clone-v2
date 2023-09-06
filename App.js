cddf30f50a3b4ef5ec28ef0b33e7605e07c49910
momo <xdankiiv3@gmail.com>
2023-09-06 11:59:06 +0200

    Sache dings dongs geht

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Start from "./compoments/Start";
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ChakraProvider>
+          <Start></Start>
+      </ChakraProvider>
+      <Text>test!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ compoments/Start.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Textarea } from '@chakra-ui/react'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+} from '@chakra-ui/react'
+
+export default function Start(props) {
+    return (
+        <Textarea placeholder='Here is a sample placeholder' />
+    )
+}
