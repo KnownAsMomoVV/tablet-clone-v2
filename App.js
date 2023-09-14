bea89ca7478cbe7f0804936dec4469b6798c139e
Momo <74142294+KnownAsMomoVV@users.noreply.github.com>
2023-09-14 23:55:15 +0200

    dark mode

--- App.js
+++ App.js
@@ -14,22 +14,53 @@ import CardDetailScreen from "./compoments/CardDetailScreen";
 
 import {Button} from "@chakra-ui/button";
 function HomeScreen({ navigation }) {
+    const [isDarkMode, setIsDarkMode] = useState(false); // State to manage dark mode
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+    const currentTheme = isDarkMode ? darkTheme : lightTheme;
     return (
-        <View style={styles.container}>
+        <View style={currentTheme.container}>
+            <Button onClick={toggleDarkMode}>
+                {isDarkMode ? '☀️' : '🌙'}
+            </Button>
             <Grid templateColumns="repeat(3, 1fr)" templateRows="repeat(3, 1fr)" gap={6}>
-            <ButtonSample text='🔜'></ButtonSample>
-            <ButtonSample text='🔜'></ButtonSample>
-            <ButtonSample text="🔜" onClick={() => navigation.navigate('Events')} />
-            <ButtonSample text="🔜" onClick={() => navigation.navigate('Calendar')} />
-            <ButtonSample text='🔜'></ButtonSample>
-            <ButtonSample text="🔜" onClick={() => window.location.href='./compoments/CalendarFirebase.js'} />
                 <ButtonSample text="📆" onClick={() => navigation.navigate('CalendarFirebase')} />
+            <ButtonSample text='🔜'></ButtonSample>
+            <ButtonSample text='🔜'></ButtonSample>
+            <ButtonSample text='🔜'></ButtonSample>
+            <ButtonSample text='🔜'></ButtonSample>
+                <ButtonSample text='🔜'></ButtonSample>
+                <ButtonSample text='🔜'></ButtonSample>
             <ButtonSample text='🔜'></ButtonSample>
             <ButtonSample text='🔜'></ButtonSample>
                 </Grid>
         </View>
     );
 }
+const lightTheme = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f4f4f8',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    // ... any other styles for light mode ...
+});
+
+const darkTheme = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#282c34',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    // ... any other styles for dark mode ...
+});
 
 export default function App() {
     useEffect(() => {
@@ -60,24 +91,31 @@ export default function App() {
     }, []);
     return (
         <ChakraBaseProvider>
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="CalendarFirebase" component={calendarFirebase} />
-                <Stack.Screen name="Calendar" component={CalendarView} />
-                <Stack.Screen name="CardDetailScreen" component={CardDetailScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Home"
+                    screenOptions={{
+                        headerStyle: { backgroundColor: '#282c34' }, // Dark background
+                        headerTintColor: '#fff', // White text
+                        headerTitleStyle: { fontWeight: 'bold' }, // Bold text for header
+                    }}
+                >
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="CalendarFirebase" component={calendarFirebase} />
+                    <Stack.Screen name="Calendar" component={CalendarView} />
+                    <Stack.Screen name="CardDetailScreen" component={CardDetailScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
         </ChakraBaseProvider>
     );
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#f4f4f8', // A soft gray background
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20, // Consistent padding for better spacing
+    },
 });
